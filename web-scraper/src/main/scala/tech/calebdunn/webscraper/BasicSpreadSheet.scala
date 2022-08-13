1efce56f9a9f9e3ef244d6889038ae67c749d140
Caleb Dunn <caldunn@outlook.com>
2022-08-13 15:03:41 +0800

    Lil Styling

--- web-scraper/src/main/scala/tech/calebdunn/webscraper/BasicSpreadSheet.scala
+++ web-scraper/src/main/scala/tech/calebdunn/webscraper/BasicSpreadSheet.scala
@@ -28,40 +28,68 @@ object BasicSpreadSheet {
     for (round <- rounds) {
       val sheet = wb.createSheet(s"round-${round.round}")
 
-      val style = wb.createCellStyle()
+      // .copy does not seem to work for some reason. Let me just use this.
+      def genBaseStyle: HSSFCellStyle =
+        val style = wb.createCellStyle()
+        style.setFillPattern(FillPatternType.THIN_FORWARD_DIAG)
+        style.setBorderBottom(BorderStyle.DASHED)
+        style
 
-      createHeader(sheet, wb)
-      var fillFlip = false
+      val rStyle1 = genBaseStyle
+      rStyle1.setFillForegroundColor(IndexedColors.LAVENDER.getIndex)
+
+      val rStyle2 = genBaseStyle
+      rStyle2.setFillForegroundColor(IndexedColors.LIME.getIndex)
+
+      val posColStyle = genBaseStyle
+      val font        = wb.createFont()
+      font.setBold(true)
+      posColStyle.setFont(font)
+
+      createHeader(sheet)
       for (score <- round.mapAsArray) {
         val row = sheet.createRow(score.pos)
-        row.createCell(0).setCellValue(score.pos)
+        val pos = row.createCell(0)
+        pos.setCellValue(score.pos)
+
         row.createCell(1).setCellValue(score.uname)
         row.createCell(2).setCellValue(score.score)
         row.createCell(3).setCellValue(score.margin)
+        val rowStyle = if score.pos % 2 == 0 then rStyle1 else rStyle2
 
+        row.cellIterator.asScala.foreach(_.setCellStyle(rowStyle))
       }
       sheet.autoSizeColumn(1)
-      fillFlip = !fillFlip
-      // Add a simple border.
 
+      // Outside border
+      val pt = PropertyTemplate()
+      pt.drawBorders(
+        CellRangeAddress(1, 15, 0, 3),
+        BorderStyle.MEDIUM,
+        IndexedColors.BLACK.getIndex,
+        BorderExtent.OUTSIDE
+      )
+
+      pt.applyBorders(sheet)
     }
 
     val file = FileOutputStream(s"./$filename")
     wb.write(file)
   }
 
-  def createHeader(sheet: HSSFSheet, book: HSSFWorkbook): Unit = {
+  def createHeader(sheet: HSSFSheet): Unit = {
     val header = sheet.createRow(0)
     header.createCell(0).setCellValue("Position")
     header.createCell(1).setCellValue("Username")
     header.createCell(2).setCellValue("Score")
     header.createCell(3).setCellValue("Margin")
 
+    val book  = sheet.getWorkbook
     val style = book.createCellStyle()
     style.setFillForegroundColor(IndexedColors.LAVENDER.getIndex)
     style.setFillPattern(FillPatternType.THIN_FORWARD_DIAG)
     style.setLocked(true)
-    style.setBorderBottom(BorderStyle.MEDIUM)
+    // style.setBorderBottom(BorderStyle.MEDIUM)
 
     val font = book.createFont()
     font.setBold(true)
@@ -72,9 +100,9 @@ object BasicSpreadSheet {
     }
     val props = PropertyTemplate()
     props.drawBorders(
-      CellRangeAddress(1, 2, 1, 3),
+      CellRangeAddress(0, 1, 0, 3),
       BorderStyle.MEDIUM,
-      IndexedColors.RED.getIndex,
+      IndexedColors.BLACK.getIndex,
       BorderExtent.ALL
     )
     props.applyBorders(sheet)
